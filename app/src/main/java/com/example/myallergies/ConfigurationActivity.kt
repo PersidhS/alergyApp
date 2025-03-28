20ddb1086590193d9aca466643dadb916a9fefa5
p3rsidh <persidhpimenta@gmail.com>
2025-03-28 18:24:16 -0300

    init

--- app/src/main/java/com/example/myallergies/ConfigurationActivity.kt
+++ app/src/main/java/com/example/myallergies/ConfigurationActivity.kt
@@ -23,7 +23,7 @@ class ConfigurationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.configuration_main)
+        setContentView(R.layout.activity_configuration)
 
         userPhotoView = findViewById(R.id.userPhotoView)
         etUserName = findViewById(R.id.etUserName)
